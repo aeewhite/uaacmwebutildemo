2d3598773ae4a69dec2b4da076dd0a6c279d7206
David <damccoy1@crimson.ua.edu>
2017-07-23 12:59:14 -0500

    simplified message

--- emailmanager.py
+++ emailmanager.py
@@ -28,13 +28,10 @@ def sendMemberList(server):
 
 def sendAcknowledgement(server, firstName, lastName, email, slack):
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Welcome to UA ACM'
+    msg['Subject'] = 'Demo Email'
     msg['From'] = FROM_EMAIL()
     msg['To'] = email
-    body = '<h2>We\'re glad to have you on board!</h2><br>'
-    if slack == '1':
-        body += '<a href=\"https://unofficialuacs.slack.com/signup?email=' + email.split('@')[0] + '\">Complete Slack registration</a><br><br>'
-    body += '<a href=\"mailto:listserv@bama.ua.edu?body=subscribe%20ua-acm%20' + firstName + '%20' + lastName + '\">Send message to listserv to get our emails</a><br>(if email links aren\'t you\'re thing, manually email listserv@bama.ua.edu with the message \'subscribe ua-acm\')'
+    body = '<h2>Hello world!</h2><br>'
     body_container = MIMEMultipart('alternative')
     body_container.attach(MIMEText(body.encode('utf-8'), 'html', 'UTF-8'))
     msg.attach(body_container)
